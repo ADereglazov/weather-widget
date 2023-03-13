79249e43cacc3da8efbe01ea885a3f165d018585
ADereglazov <sanya95@yandex.ru>
2023-03-13 18:25:42 +0300

    added ts types to function setLocalStorageSettings

--- src/WeatherWidget.ce.vue
+++ src/WeatherWidget.ce.vue
@@ -184,13 +184,7 @@ function onManageButtonClick() {
     errStatus.value = dict[props.lang].noDataMessage;
   }
 }
-function onChangeSettings({
-  lang = props.lang,
-  units = props.units,
-}: {
-  lang: TLanguage;
-  units: TUnits;
-}) {
+function onChangeSettings({ lang, units }: { lang: TLanguage; units: TUnits }) {
   setLocalStorageSettings({ lang, units });
   props.lang = lang;
   props.units = units;
