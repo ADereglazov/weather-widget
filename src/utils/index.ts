1b1cce9baf8cb8a23f891a255602280f0ae28e7c
ADereglazov <sanya95@yandex.ru>
2023-04-03 22:25:27 +0300

    delete modifyMatchText

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,6 @@
 export * from "./getOutdatedWeatherLocationIndexes";
 export * from "./getWindDirection";
 export * from "./testLocalStorage";
-export * from "./modifyMatchText";
 export * from "./scrollSelectionIntoView";
 export * from "./pluralize";
 export * from "./capitalizeFirstLetter";
